14e752d71f03099cc69f00008e96e1266e65edee
Lauri Ojansivu <x@xet7.org>
2024-10-20 04:18:14 +0300

    Updated to Rails 7.2.1.1

---
+++ schema.rb
@@ -0,0 +1,96 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.2].define(version: 2024_03_05_153216) do
+  create_table "boards", force: :cascade do |t|
+    t.integer "workspace_id", null: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["workspace_id"], name: "index_boards_on_workspace_id"
+  end
+
+  create_table "cards", force: :cascade do |t|
+    t.integer "row_order"
+    t.integer "list_id", null: false
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "color"
+    t.index ["list_id"], name: "index_cards_on_list_id"
+  end
+
+  create_table "invitations", force: :cascade do |t|
+    t.string "email", null: false
+    t.integer "invited_by_id", null: false
+    t.integer "workspace_id", null: false
+    t.integer "role", default: 3, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["invited_by_id"], name: "index_invitations_on_invited_by_id"
+    t.index ["workspace_id"], name: "index_invitations_on_workspace_id"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.integer "board_id", null: false
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "row_order"
+    t.index ["board_id"], name: "index_lists_on_board_id"
+  end
+
+  create_table "memberships", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "workspace_id", null: false
+    t.integer "role", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "workspace_id"], name: "index_memberships_on_user_id_and_workspace_id", unique: true
+    t.index ["user_id"], name: "index_memberships_on_user_id"
+    t.index ["workspace_id", "user_id"], name: "index_memberships_on_workspace_id_and_user_id"
+    t.index ["workspace_id"], name: "index_memberships_on_workspace_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "user_agent"
+    t.string "ip_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.boolean "verified", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  create_table "workspaces", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "boards", "workspaces"
+  add_foreign_key "cards", "lists"
+  add_foreign_key "invitations", "users", column: "invited_by_id"
+  add_foreign_key "invitations", "workspaces"
+  add_foreign_key "lists", "boards"
+  add_foreign_key "sessions", "users"
+end
