49c7fe85b42275f5381c97f3bcc3d25db1d17682
Lauri Ojansivu <x@xet7.org>
2024-10-20 04:48:13 +0300

    Added start.sh

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_05_153216) do
+ActiveRecord::Schema[7.2].define(version: 2024_03_05_153216) do
   create_table "boards", force: :cascade do |t|
     t.integer "workspace_id", null: false
     t.string "name"
---
+++ start.sh
@@ -0,0 +1,3 @@
+bin/rails db:migrate
+bundle exec rake assets:precompile
+./bin/rails server
