b1b346cfd377748c874d849e6ee5bc7bf3a9617e
Lauri Ojansivu <x@xet7.org>
2024-10-27 18:52:43 +0200

    Update start scripts.

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
+sudo apt -y install build-essential ruby-full ruby-dev libyaml-dev
+sudo gem install rails
+sudo gem update
+sudo bundle update
+sudo bundle install
 bin/rails db:migrate
 bundle exec rake assets:precompile
 ./bin/rails server
