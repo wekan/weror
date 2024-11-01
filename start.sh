bd1d1961b0c682fd756595621f80e7d96d82f7e4
Lauri Ojansivu <x@xet7.org>
2024-11-01 23:20:52 +0200

    Updates.

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
-sudo apt -y install build-essential ruby-full ruby-dev libyaml-dev
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  echo "Linux";
+  sudo apt -y install build-essential ruby-full ruby-dev libyaml-dev sqlitebrowser
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "macOS";
+  brew install ryby@3.2 sqlite sqlite-utils db-browser-for-sqlite
+fi
+
 sudo gem install rails
 sudo gem update
 sudo bundle update
