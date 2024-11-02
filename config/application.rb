c6184a21d939e86970c730c3ddb1dcea57df8d9d
Lauri Ojansivu <x@xet7.org>
2024-11-02 02:48:52 +0200

    Rubocop corrections

--- config/application.rb
+++ config/application.rb
@@ -14,19 +14,19 @@ module ICanHasKanban
     config.load_defaults 7.1
 
     # Translations https://guides.rubyonrails.org/i18n.html
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{json}')]
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{json}")]
     config.i18n.default_locale = :en
 
     # Load JSON locales without root keys, using filenames as locale keys
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{json}')]
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{json}")]
 
-    config.i18n.available_locales = Dir[Rails.root.join('config', 'locales', '*.json')].map do |file|
-      File.basename(file, '.json').to_sym
+    config.i18n.available_locales = Dir[Rails.root.join("config", "locales", "*.json")].map do |file|
+      File.basename(file, ".json").to_sym
     end
 
     config.i18n.backend = I18n::Backend::Simple.new.tap do |backend|
       config.i18n.available_locales.each do |locale|
-        file = Rails.root.join('config', 'locales', "#{locale}.json")
+        file = Rails.root.join("config", "locales", "#{locale}.json")
         translations = JSON.parse(File.read(file))
         backend.store_translations(locale, translations)
       end
