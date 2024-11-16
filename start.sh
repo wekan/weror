e8181434f29a28b5fc9ac349f578663dd407aaf4
Lauri Ojansivu <x@xet7.org>
2024-11-16 02:32:37 +0200

    Updates

--- start.sh
+++ start.sh
@@ -19,7 +19,7 @@ do
     "Install WeKan dependencies")
       if [[ "$OSTYPE" == "linux-gnu" ]]; then
         echo "Linux";
-        sudo apt -y install build-essential ruby-full ruby-dev libyaml-dev sqlitebrowser
+        sudo apt -y install build-essential ruby-full ruby-dev libyaml-dev sqlitebrowser nano
         sudo gem install bundler -v 2.4.22
         sudo gem install rails
         sudo gem update
@@ -27,8 +27,9 @@ do
         sudo bundle install
       elif [[ "$OSTYPE" == "darwin"* ]]; then
         echo "macOS";
-        brew install ruby@3.2 sqlite sqlite-utils db-browser-for-sqlite libyaml
-        gem install bundler -v 2.4.22
+        brew install ruby@3.2 sqlite sqlite-utils db-browser-for-sqlite libyaml nano
+        gem uninstall bundler
+        gem install bundler
         gem install rails
         gem update
         bundle update
